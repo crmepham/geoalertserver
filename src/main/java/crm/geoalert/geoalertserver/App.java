928be6ee736e8087474c2a95fbb5b29b0aa0b171
crm <crmepham@hotmail.co.uk>
2015-12-16 16:39:46 +0000

    init

---
+++ src/main/java/crm/geoalert/geoalertserver/App.java
@@ -0,0 +1,13 @@
+package crm.geoalert.geoalertserver;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
